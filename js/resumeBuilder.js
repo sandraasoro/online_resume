b21509396065034fa8139a489ff5138e927452b9
sandraasoro <sandy084life@yahoo.com>
2017-01-04 16:28:33 -0500

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,11 +2,12 @@ var bio = {
 	"name": "Sandra Asoro",
 	"role": "Network Design Enginner & Entry- Level Front-End Developer",
 	"contacts": {
-		"email": "sandraasoro@gmail.com",
+    "mobile": "678-488-5903",
+    "email": "sandraasoro@gmail.com",
 		"github": "sandraasoro",
 		"location": "Atlanta, GA, USA"
 	},
-	"picture": "images/profile.png",
+	"bioPic": "images/profile.png",
 	"welcomeMessage": "Hello! Thanks for viewing my page! I am a network design engineer and an entry level front-end developer.",
 	"skills": ["Project Management", "RAN Engineering", "Process Development", "Java", "Python", "Software Development"],
   "display" : function(){
@@ -17,19 +18,20 @@ var work = {
 		"employer": "AT&T",
 		"title": "Sr. Specialist- Network Design Engineer",
 		"location": "Atlanta, GA, USA",
-		"datesWorked": "July 2013-March 2015",
+		"dates": "July 2013-March 2015",
 		"description": "Engaged a variety of key stakeholder organizations and work cross-functionally to define and" + "develop process flows to identify workflow for major Network Technology Development (NTD) or manual projects" + "such as: LTE Broadcast, IP Interoperability and Wireless to Wireless Digital Signal 3 (DS3) Connectivity. Deliver" + "end to end process flows to VP organizations which as a result reduces cycle times, reduces cost and increases efficiency"
 	}, {
 		"employer": "AT&T",
 		"title": "Specialist- RAN Engineer",
 		"location": "Atlanta, GA, USA",
-		"datesWorked": "March 2015-December 2016",
+		"dates": "March 2015-December 2016",
 		"description": "Championed data quality across a variety of source databases (CSSng, ATOLL, OPTIMA, NDR, etc.)" + "by engaging market RAN and ensuring optimal performance. Developed automated soft tools in-house" + "to aid Radio Access Network (RAN) group in keeping up with new 4G network, reduce the existing 2G" + "Network and measure compliance."
 	}, {
 		"employer": "InComm",
 		"title": "System Analyst- Intern",
+
 		"location": "Atlanta, GA, USA",
-		"datesWorked": "May 2011-August 2011",
+		"dates": "May 2011-August 2011",
 		"description": "Led intern group to work directly with merchants to understand the various" + "InComm products by engaging them in training sessions and developing job aid documents." + "Worked on technical document for B2B OpenLoop Project which explained to customers the" + "main advantage of investing in open loop gift cards such as Visa.",
     "display" : function(){
     }
@@ -41,32 +43,32 @@ var education = {
 		"datesAttended": "2008-2013",
 		"location": "Marietta, GA, USA",
 		"degree": "B.S. Electrical Engineering",
-		"major": "Electrical Engineering",
+		"majors": "Electrical Engineering",
 		"url": "www.kennesaw.edu"
 	}],
 	"onlineCourses": [{
 		"school": "Udacity",
 		"title": "Introduction to Programming",
-		"completed": "December 2016",
+		"dates": "December 2016",
 		"url": "https://www.udacity.com"
 	}]
 };
 var projects = {
 	"projects": [{
 		"title": "Make a Webpage",
-		"datesWorked": "August 2015",
+		"dates": "August 2015",
 		"description": "Created a web page using HTML and CSS and applied it to skills learned" + "Nanodegree.",
 		"images": ["images/webpage.jpg"],
 		"url": "https://github.com/sandraasoro/Web-Page-Dev"
 	}, {
 		"title": "Code Your Own Quiz",
-		"datesWorked": "November 2016",
+		"dates": "November 2016",
 		"description": "Created a fill in the blank style quiz using the Python programming language" + "Nanodegree.",
 		"images": ["images/quizgame.jpg"],
 		"url": "https://github.com/sandraasoro/code_your_own_quiz"
 	}, {
 		"title": "Create a Movie Website",
-		"datesWorked": "December 2016",
+		"dates": "December 2016",
 		"description": "Wrote code to store list of favorite movies and used code to generate a static web page allowing visitors to browse and watch trailers.",
 		"images": ["images/moviesite.jpg"],
 		"url": "https://github.com/sandraasoro/create_a_movie_website"
@@ -75,9 +77,10 @@ var projects = {
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
 var formattedContactInfo = [];
+formattedContactInfo.push(HTMLmobile.replace("%data%", bio.contacts.mobile));
 formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
 formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
 formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
@@ -104,12 +107,12 @@ function displayWork() {
 			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
 			var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
 			var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
-			var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[i].datesWorked);
+			var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
 			var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
 			var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
 			$(".work-entry:last").append(formattedEmployerWorkTitle);
 			$(".work-entry:last").append(formattedWorkLocation);
-			$(".work-entry:last").append(formattedDatesWorked);
+			$(".work-entry:last").append(formattedDates);
 			$(".work-entry:last").append(formattedWorkDescription);
 		}
 	}
@@ -120,7 +123,7 @@ projects.display = function() {
 		for (var i = 0; i < projects.projects.length; i++) {
 			$("#projects").append(HTMLprojectStart);
 			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
-			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].datesWorked);
+			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
 			var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
 			$(".project-entry:last").append(formattedProjectTitle);
 			$(".project-entry:last").append(formattedProjectDates);
@@ -143,11 +146,11 @@ education.display = function() {
 			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
 			var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].datesAttended);
 			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
-			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+			var formattedSchoolMajors = HTMLschoolMajors.replace("%data%", education.schools[i].majors);
 			$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
 			$(".education-entry:last").append(formattedSchoolDates);
 			$(".education-entry:last").append(formattedSchoolLocation);
-			$(".education-entry:last").append(formattedSchoolMajor);
+			$(".education-entry:last").append(formattedSchoolMajors);
 		}
 		if (education.onlineCourses.length > 0) {
 			for (i = 0; i < education.onlineCourses.length; i++) {
@@ -155,7 +158,7 @@ education.display = function() {
 				$("#education").append(HTMLschoolStart);
 				var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
 				var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
-				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].completed);
+				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
 				var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
 				$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
 				$(".education-entry:last").append(formattedOnlineDates);
@@ -165,4 +168,5 @@ education.display = function() {
 	}
 };
 education.display();
+
 $("#mapDiv").append(googleMap);
